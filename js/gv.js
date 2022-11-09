6265ffdb9a4e57428f91b3e7d1100ec398fd85f9
Green Vision Media <113368396+greenvisionmedia@users.noreply.github.com>
2022-11-09 10:46:15 -0700

    minified, changed carbon data, open graph

--- js/gv.js
+++ js/gv.js
@@ -1,53 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Query shorthands
-    const get = (i) => document.getElementById(i);
-    const toggle = (e) => {
-        e.classList.toggle('on');
-    };
+// Query shorthands
+const get = (i) => document.getElementById(i);
+const toggle = (e) => {
+    e.classList.toggle('on');
+};
 
-    let contact = get('contact'),
-        trigger = get('trigger'),
-        formBox = get('form-box');
+let contact = get('contact'),
+    trigger = get('trigger'),
+    formBox = get('form-box');
 
-    // Contact appear
-    new IntersectionObserver(
-        (entries, observer) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    toggle(contact);
-                    toggle(formBox);
-                    observer.disconnect();
-                }
-            });
-        },
-        {
-            rootMargin: '16px',
-        }
-    ).observe(trigger);
-
-    // BASIN
-
-    let form = get('form'),
-        success = get('succ-box'),
-        failure = get('fail-box');
-
-    form.onsubmit = function (event) {
-        event.preventDefault();
-
-        let formData = new FormData(form),
-            xhr = new XMLHttpRequest();
-
-        xhr.open('POST', form.action, true);
-        xhr.send(formData);
-        xhr.onload = function (e) {
-            if (xhr.status === 200) {
+// Contact appear
+new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                toggle(contact);
                 toggle(formBox);
-                toggle(success);
-            } else {
-                var response = JSON.parse(xhr.response);
-                toggle(formBox);
-                toggle(failure);
+                observer.disconnect();
             }
-        };
+        });
+    },
+    {
+        rootMargin: '16px',
+    }
+).observe(trigger);
+
+// BASIN
+
+let form = get('form'),
+    success = get('succ-box'),
+    failure = get('fail-box');
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+
+    let formData = new FormData(form),
+        xhr = new XMLHttpRequest();
+
+    xhr.open('POST', form.action, true);
+    xhr.send(formData);
+    xhr.onload = function (e) {
+        if (xhr.status === 200) {
+            toggle(formBox);
+            toggle(success);
+        } else {
+            var response = JSON.parse(xhr.response);
+            toggle(formBox);
+            toggle(failure);
+        }
     };
-});
+};
